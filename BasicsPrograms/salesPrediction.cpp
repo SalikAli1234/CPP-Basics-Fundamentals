745c8833c9bdf6a742a2a1d9e88a13c882ae7a51
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-04 00:20:59 +0500

    Create salesPrediction.cpp

---
+++ BasicsPrograms/salesPrediction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+    using namespace std;
+    int main()
+    {
+        long double sales=8.6E6;
+        float percentage=0.56;
+        long double predict_division=0;
+        predict_division=sales*percentage;
+        cout<<"The predicted sales percentage = "<<predict_division<<endl;
+
+        return 0;
+    }
